273a3efa1021fd170afb7ed6d707e8ae8dd980f0
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-18 21:21:10 -0300

    segundo requisito testado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const ageGroup = data.species.filter((item) => item.name === animal);
+  const { residents } = ageGroup[0];
+  const booleanList = residents.map((res) => res.age >= age);
+  const result = booleanList.some((item) => item === false);
+  if (result) {
+    return false;
+  }
+  return true;
 };
+console.log(getAnimalsOlderThan('lions', 12));
 
 module.exports = getAnimalsOlderThan;
