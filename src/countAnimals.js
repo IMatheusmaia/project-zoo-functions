38ef80470fea2f6175e9e3884332d8b335bb3efd
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-20 22:33:04 -0300

    requisito 5 testado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAllAnimals = () => {
+  const { species } = data;
+  const object = { };
+  species.forEach((specie) => {
+    object[specie.name] = specie.residents.length;
+  });
+  return object;
 };
+const countBySpecie = (animal) => {
+  const { species } = data;
+  const specie = species.find((spp) => spp.name === animal.species);
+  // const { name } = specie;
+  const { residents } = specie;
+  // const object = {};
+  // object[name] = residents.length;
+  return residents.length;
+};
+const countBySex = (animal) => {
+  const { species } = data;
+  const specie = species.find((spp) => spp.name === animal.species);
+  // const { name } = specie;
+  const { residents } = specie;
+  const female = residents.filter((res) => res.sex === animal.sex);
+  // const object = {};
+  // object[name] = female.length;
+  return female.length;
+};
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    return countAllAnimals();
+  }
+  if (animal.species && animal.sex) {
+    return countBySex(animal);
+  }
+  if (animal.species) {
+    return countBySpecie(animal);
+  }
+};
+console.log(countAnimals());
 
 module.exports = countAnimals;
