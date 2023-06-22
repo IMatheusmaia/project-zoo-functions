518be4c93a40a8093535a79285df2edc82aeec15
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-22 10:24:56 -0300

    cobertura de teste do handlerElephants a 100%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,26 @@
-const handlerElephants = require('../src/handlerElephants');
+const { getElephants, averageAge, computeData, handlerElephants } = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+const elephantObj = getElephants();
+
+describe('Testes da função HandlerElephants', () => {
+  it('verificando se a função getElephants retorna o objeto correto', () => {
+    expect(getElephants()).toEqual(elephantObj);
+  });
+  it('Verifica se na função computeData retorna o valor correto para os respectivos parâmetros passados', () => {
+    expect(computeData('count', elephantObj)).toBe(4);
+    expect(computeData('names', elephantObj)).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(computeData('averageAge', elephantObj)).toBeCloseTo(10.5);
+    expect(computeData('sex', elephantObj)).toBe(null);
+  });
+  it('Verifica se na função handlerElephants retorna o valor correto para os respectivos parâmetros passados', () => {
+    expect(handlerElephants()).toBe(undefined);
+    expect(handlerElephants(12)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants('weight')).toBe(null);
+  });
+  it('Verifica se a função average retorna o valor correto', () => {
+    expect(averageAge(elephantObj)).toBeCloseTo(10.5);
+  });
+});
