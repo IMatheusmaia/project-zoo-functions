9206384d70101fe3ff9308c8d284e7a24e328a2a
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-22 10:51:50 -0300

    tentando revalidar requisitos 6 e 7

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
 };
 
 module.exports = { calculateEntry, countEntrants };
