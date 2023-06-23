619735f314ea97b11674f3c381e2eeee24447bf0
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-06-23 16:05:20 -0300

    requisito 10 testado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employer = employees.find((person) => person.id === id);
+  if (employer === undefined) {
+    return 'Id de funcionário não encontrado';
+  }
+  const { responsibleFor } = employer;
+  const ageList = species.find((animal) => animal.id === responsibleFor[0]);
+  const older = ageList.residents.sort((a, b) => b.age - a.age);
+  return Object.values(older[0]);
 };
+console.log(getOldestFromFirstSpecies());
 
 module.exports = getOldestFromFirstSpecies;
